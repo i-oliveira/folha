22ee6cd2e6f524abbf8d8194155f5601d925168a
i-oliveira <50674376+i-oliveira@users.noreply.github.com>
2019-05-15 14:24:23 -0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,26 @@
+<html>
+    <meta charset="UTF-8">
+    <head>
+        <title>Campo de Formulário</title>
+        
+	<link rel="stylesheet" href="style.css" type="text/css">    
+    </head>
+    <body>
+		<div>
+			<center>
+			
+            <h2 style="color:black;">FORMULÁRIO</h2>
+			<div id="form">
+				<form method="POST"  action="respostas.php">
+                </br></br>Nome</br>
+                <input type="text" name="nome"/>
+                </br></br>Horas Trabalhadas</br>
+                <input type="text" name="horas"/>
+				</br></br>Valor da Hora</br>
+                <input type="text" name="valor"/>
+                </br></br>
+                <input type="submit">
+				</form>
+		</div>
+    </body>
+</html>
